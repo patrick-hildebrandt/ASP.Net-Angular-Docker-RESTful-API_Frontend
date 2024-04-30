98ea6b12991515066b579da538310b048e33ed3c
antischock <102297813+patrick-hildebrandt@users.noreply.github.com>
2024-04-30 21:42:41 +0200

    frontend calls api - backend still blocks

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+interface Article {
+  artikelnummer: string;
+  marke: string;
+  material1: string;
+  legierung1: string;
+  kollektion: string;
+  warengruppe: string;
+  warenhauptgruppe: string;
+  geschlecht: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'Frontend';
+  articles: Article[] = [];
+  // test: any;
+
+  constructor(private http: HttpClient) {
+  }
+
+  fetchArticles() {
+    this.http.get<Article[]>('http://localhost:32771/api/articles').subscribe(
+    // this.http.get<any>('https://jsonplaceholder.typicode.com/todos/1').subscribe(
+      (resp: any) => {
+        console.log(resp);
+        this.articles = resp;
+        // this.test = resp;
+      }
+    );
+  }
+
+  ngOnInit(): void {
+    this.fetchArticles();
+  }
 }
